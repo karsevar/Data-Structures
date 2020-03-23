815f971dd927137dab3fb816a51fab961eacbc36
masonkarsevar <masonkarsevar@gmail.com>
2020-03-23 10:58:29 -0700

    added max method to doubly linked list

--- doubly_linked_list/doubly_linked_list.py
+++ doubly_linked_list/doubly_linked_list.py
@@ -118,4 +118,14 @@ class DoublyLinkedList:
         
     """Returns the highest value currently in the list"""
     def get_max(self):
-        pass
+        if not self.head:
+            return None 
+        max_value = self.head.value 
+        current = self.head 
+
+        while current:
+            if current.value > max_value:
+                max_value = current.value 
+            current = current.next
+
+        return max_value
